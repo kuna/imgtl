4d62ea3e857e95e5265f15279c759e96ce269e4f
JuneHyeon Bae <devunt@gmail.com>
2013-08-29 07:29:53 +0900

    implements mypage image grid

--- static/js/mypage.js
+++ static/js/mypage.js
@@ -1,3 +1,11 @@
 $(function () {
-
+    $(".grid-item").mouseover(function () {
+        $(this).animate({
+            "border": "solid 1px rgba(49, 176, 213, 1)"
+        }, 500);
+    }).mouseout(function (){
+        $(this).animate({
+            "border": "solid 1px rgba(49, 176, 213, 0)"
+        }, 500);
+    });
 });
