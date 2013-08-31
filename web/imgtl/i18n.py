5392fdd9cf50e162af06e4ed426d82cd3030cad3
JuneHyeon Bae <devunt@gmail.com>
2013-08-31 18:08:32 +0900

    add missing i18n text

--- web/imgtl/i18n.py
+++ web/imgtl/i18n.py
@@ -5,8 +5,9 @@ i18n_dict = {
         'ko': {
             'youmustlogin': '로그인을 해야 합니다',
             'invalidemail': '잘못된 이메일 주소입니다.',
-            'invalidusername': '잘못된 사용자명입니다.',
-            'passwordmismatch': '비밀번호가 일치하지 않습니다.',
+            'invalidusername': '잘못된 사용자명입니다. 사용자명은 최소 4글자여야 합니다',
+            'invalidpassword': '잘못된 비밀번호입니다. 비밀번호는 최소 8글자여야 합니다.',
+            'passwordmismatch': '두 비밀번호가 일치하지 않습니다.',
             'alreadyexistemail': '이미 존재하는 이메일입니다.',
             'alreadyexistname': '이미 존재하는 사용자 이름입니다.',
             'loginfailed': '잘못된 이메일, 계정명 또는 비밀번호입니다.',
