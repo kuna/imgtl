173f0c75087f1213af88d762dbd69a07dd149a00
JuneHyeon Bae <devunt@gmail.com>
2013-09-22 16:47:13 +0900

    added http status code in api, added /user/info api endpoint

--- web/api.py
+++ web/api.py
@@ -39,40 +39,51 @@ class Upload(Resource):
         if user:
             user = User.query.filter_by(token=user).first()
             if not user:
-                return error('wrongtoken')
+                return error('wrongtoken'), 403
         if 'image' not in request.files:
-            return error('imagenotattached')
+            return error('imagenotattached'), 400
         upload = do_upload_image(user, request.files['image'], request.form['desc'] if 'desc' in request.form else None)
         if isinstance(upload, str):
-            return error(upload)
+            return error(upload), 403
         else:
-            return success({'url': {'page': 'https://img.tl/%s' % upload.url, 'direct': 'https://img.tl/%s.%s' % (upload.url, upload.object.ext), 'original': upload.object.original_url}})
+            return success({'url': {'page': 'https://img.tl/%s' % upload.url, 'direct': 'https://img.tl/%s.%s' % (upload.url, upload.object.ext), 'original': upload.object.original_url}}), 201
 
 
 class Url(Resource):
     def get(self, url):
         upload = imgtl.db.Upload.query.filter_by(url=url).first()
         if not upload or upload.deleted:
-            return error('nosuchupload')
+            return error('nosuchupload'), 404
         user = {'name': upload.user.name, 'profile_image_url': upload.user.profile_image_url} if upload.user else None
         return success({'url': {'page': 'https://img.tl/%s' % upload.url, 'direct': 'https://img.tl/%s.%s' % (upload.url, upload.object.ext), 'original': upload.object.original_url}, 'title': upload.title, 'desc': upload.desc, 'upload_at': upload.time.strftime('%s'), 'user': user, 'view_count': upload.view_count})
 
     def delete(self, url):
         token = request.headers.get('X-IMGTL-TOKEN')
         if not token:
-            return error('notoken')
+            return error('notoken'), 403
         user = User.query.filter_by(token=token).first()
         if not user:
-            return error('wrongtoken')
+            return error('wrongtoken'), 403
         res = do_delete_image(user, url)
         if res == 'success':
             return {'status': 'success'}
         else:
-            return error(res)
+            return error(res), 403
+
+class UserInfo(Resource):
+    def get(self):
+        token = request.headers.get('X-IMGTL-TOKEN')
+        if not token:
+            return error('notoken'), 403
+        user = User.query.filter_by(token=token).first()
+        if not user:
+            return error('wrongtoken'), 403
+        return {'name': user.name, 'email': user.email, 'profile_image_url': user.profile_image_url, 'uploads_count': len(user.uploads.all())}
 
 
 api.add_resource(Upload, '/upload')
 api.add_resource(Url, '/url/<string:url>')
+api.add_resource(UserInfo, '/user/info')
 
 if __name__ == '__main__':
     db.create_all()
